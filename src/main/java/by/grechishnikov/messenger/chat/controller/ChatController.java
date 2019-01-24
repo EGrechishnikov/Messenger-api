d2ba132e0b239cb58b9c4e2ba427ddf131b996ab
EGrehishnikov <ke6a93@gmail.com>
2019-01-24 16:55:05 +0300

    refactoring

--- src/main/java/by/grechishnikov/messenger/chat/controller/ChatController.java
+++ src/main/java/by/grechishnikov/messenger/chat/controller/ChatController.java
@@ -2,7 +2,11 @@ package by.grechishnikov.messenger.chat.controller;
 
 import by.grechishnikov.messenger.chat.entity.Chat;
 import by.grechishnikov.messenger.chat.service.ChatService;
+import by.grechishnikov.messenger.message.entity.Message;
+import by.grechishnikov.messenger.message.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +21,12 @@ import java.util.List;
 public class ChatController {
 
     private ChatService chatService;
+    private MessageService messageService;
 
     @Autowired
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, MessageService messageService) {
         this.chatService = chatService;
+        this.messageService = messageService;
     }
 
     @PostMapping
@@ -37,6 +43,15 @@ public class ChatController {
     public ResponseEntity<List<Chat>> findChatsByUserId(@PathVariable int userId) {
         try {
             return new ResponseEntity<>(chatService.findAllByUserId(userId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{chatId}/messages")
+    public ResponseEntity<Page<Message>> findMessagesByChatId(@PathVariable int chatId, Pageable pageable) {
+        try {
+            return new ResponseEntity<>(messageService.findAllByChatId(chatId, pageable), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/main/java/by/grechishnikov/messenger/message/controller/MessageController.java
+++ src/main/java/by/grechishnikov/messenger/message/controller/MessageController.java
@@ -3,8 +3,6 @@ package by.grechishnikov.messenger.message.controller;
 import by.grechishnikov.messenger.message.entity.Message;
 import by.grechishnikov.messenger.message.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,15 +28,6 @@ public class MessageController {
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/{chatId}")
-    public ResponseEntity<Page<Message>> findAllByChatId(@PathVariable int chatId, Pageable pageable) {
-        try {
-            return new ResponseEntity<>(messageService.findAllByChatId(chatId, pageable), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
