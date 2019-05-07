ee7f1238dfec8bf0b824885176903b206843d50c
MacBook <ke6a93@gmail.com>
2019-05-07 16:36:40 +0300

    swagger, sql update

--- src/main/resources/DB.sql
+++ src/main/resources/DB.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS t_attachment (
 
 CREATE TABLE IF NOT EXISTS t_user (
     id                    serial NOT NULL,
-    login                 varchar(50) NOT NULL UNIQUE,
+    login                 varchar(50) NOT NULL,
     password              varchar(255) NOT NULL,
     name                  varchar(255),
     failed_login_attempts smallint,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS t_user (
     CONSTRAINT t_attachment_fk FOREIGN KEY (avatar_id) REFERENCES t_attachment (id)
 );
 
-CREATE INDEX IF NOT EXISTS login_index ON t_user (login);
+CREATE UNIQUE INDEX IF NOT EXISTS user_login_index ON t_user (login);
 
 CREATE TABLE IF NOT EXISTS t_chat (
     id      serial NOT NULL,
@@ -37,9 +37,9 @@ CREATE TABLE IF NOT EXISTS t_chat_user (
     chat_id int NOT NULL,
     user_id int NOT NULL,
 
-    CONSTRAINT t_chat_user PRIMARY KEY (chat_id, user_id),
-    CONSTRAINT t_chat_fk   FOREIGN KEY (chat_id) REFERENCES t_chat (id),
-    CONSTRAINT t_user_id   FOREIGN KEY (user_id) REFERENCES t_user (id)
+    CONSTRAINT t_chat_user_pk PRIMARY KEY (chat_id, user_id),
+    CONSTRAINT t_chat_fk      FOREIGN KEY (chat_id) REFERENCES t_chat (id),
+    CONSTRAINT t_user_fk      FOREIGN KEY (user_id) REFERENCES t_user (id)
 );
 
 CREATE TABLE IF NOT EXISTS t_message (
@@ -55,4 +55,4 @@ CREATE TABLE IF NOT EXISTS t_message (
     CONSTRAINT t_user_fk FOREIGN KEY (from_user_id) REFERENCES t_user (id)
 );
 
-CREATE INDEX IF NOT EXISTS chat_id_index ON t_message (chat_id)
+CREATE INDEX IF NOT EXISTS chat_id_index ON t_message (chat_id);
